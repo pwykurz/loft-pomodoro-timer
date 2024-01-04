2175393665ab48dc5686c9677afce8351fd24e65
Pawel Wykurz <pawel.wykurz@gmail.com>
2024-01-04 16:02:17 +0100

    feat: storage

--- src/components/molecules/PlayButton/PlayButton.tsx
+++ src/components/molecules/PlayButton/PlayButton.tsx
@@ -4,14 +4,20 @@ import {FC, useState} from "react";
 import styles from './PlayButton.module.scss'
 
 export type Props = {
-  showPlayButoon?: boolean
   onClick: () => void
 }
-const PlayButton:FC<Props> = ({onClick, showPlayButoon = true})  => {
+const PlayButton:FC<Props> = ({onClick})  => {
+  const [isPlaying, setIsPlaing] = useState(false)
+
+  const _onClick = () => {
+    setIsPlaing(prevState => !prevState)
+    onClick()
+  }
+
   return (
     <div className={styles.playpause}>
-      <input id="playpause" type="checkbox"  name="check" checked={showPlayButoon} readOnly/>
-      <label htmlFor="playpause" tabIndex={1} onClick={onClick}/>
+      <input id="playpause" type="checkbox"  name="check" checked={isPlaying} readOnly/>
+      <label htmlFor="playpause" tabIndex={1} onClick={_onClick}/>
     </div>
   );
 }

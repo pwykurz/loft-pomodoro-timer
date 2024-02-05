501b2be19477840c2f28cea8df168ef8529c7a60
Pawel Wykurz <pawel.wykurz@gmail.com>
2024-02-05 13:22:15 +0100

    feat: polishing mobile version

--- src/components/molecules/ButtonWithIcon/ButtonWithIcon.tsx
+++ src/components/molecules/ButtonWithIcon/ButtonWithIcon.tsx
@@ -18,7 +18,7 @@ export const testId = createTestIdObject('ButtonWithIcon', {
 })
 const ButtonWithIcon: FC<Props> = ({ icon, children, className, ...props}) => {
   return (
-    <Button className={cn(styles.primary, 'mr-2', className)} data-testid={testId.button} {...props}>
+    <Button className={cn(styles.primary, 'mr-2 last:mr-0', className)} data-testid={testId.button} {...props}>
       <span className="mr-2 h-4 w-4">{icon}</span> {children}
     </Button>
   )

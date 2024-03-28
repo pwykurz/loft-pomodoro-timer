11bfdb65c3c87409142ebf2f544105f3872a3afc
Pawel Wykurz <pawel.wykurz@gmail.com>
2024-03-28 14:13:37 +0100

    feat: footer

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,88 +1,86 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "next/core-web-vitals"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  ignorePatterns: ['*/ui/icons/*'],
+  rules: {
+    semi: ['error', 'never'],
+    'import/no-extraneous-dependencies': 0,
+    'max-len': ['error', { code: 160 }],
+    'no-underscore-dangle': 0,
+    'prefer-destructuring': [1, { object: true, array: true }],
+    'no-restricted-syntax': [
+      'warn',
+      {
+        selector: 'TSEnumDeclaration',
+        message: "Don't declare enums", // avoid using enums - https://stackoverflow.com/questions/40275832/typescript-has-unions-so-are-enums-redundant
+      },
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
     ],
-    "ignorePatterns": ["*/ui/icons/*"],
-    "rules": {
-        "semi": ["error", "never"],
-        'import/no-extraneous-dependencies': 0,
-        'max-len': ['error', { code: 160 }],
-        'no-underscore-dangle': 0,
-        'prefer-destructuring': [1, { object: true, array: true }],
-        'no-restricted-syntax': [
-            'warn',
-            {
-                selector: 'TSEnumDeclaration',
-                message: "Don't declare enums", // avoid using enums - https://stackoverflow.com/questions/40275832/typescript-has-unions-so-are-enums-redundant
-            },
-        ],
-        'import/extensions': [
-            'error',
-            'ignorePackages',
-            {
-                js: 'never',
-                jsx: 'never',
-                ts: 'never',
-                tsx: 'never',
-            },
-        ],
-        'import/order': [
-            'error',
-            {
-                groups: ['builtin', 'external', 'internal'],
-                pathGroups: [
-                    {
-                        pattern: 'react',
-                        group: 'external',
-                        position: 'before',
-                    },
-                ],
-                pathGroupsExcludedImportTypes: ['react'],
-                'newlines-between': 'always',
-                alphabetize: {
-                    order: 'asc',
-                    caseInsensitive: true,
-                },
-            },
-        ],
-        'no-await-in-loop': 0,
-        'class-methods-use-this': 0,
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }], // to allow to use tsx files
-        'react/jsx-sort-props': 2,
-        'react/react-in-jsx-scope': 'off',
-        'react/function-component-definition': 0,
-        'react/jsx-props-no-spreading': 0,
-        'react/require-default-props': 0,
-        'testing-library/prefer-screen-queries': 'off',
-        'react-hooks/exhaustive-deps': 0,
-    }
+    'no-await-in-loop': 0,
+    'class-methods-use-this': 0,
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ], // to allow to use tsx files
+    'react/jsx-sort-props': 2,
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/require-default-props': 0,
+    'testing-library/prefer-screen-queries': 'off',
+    'react-hooks/exhaustive-deps': 0,
+  },
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 'use client'
 import { Roboto_Flex } from 'next/font/google'
-import {RecoilRoot} from "recoil"
+import { RecoilRoot } from 'recoil'
 
-import MainPage from "@/components/organisms/MainPage"
-import YTPlayer from "@/components/organisms/YTPlayer"
+import Footer from '@/components/molecules/Footer'
+import MainPage from '@/components/organisms/MainPage'
+import YTPlayer from '@/components/organisms/YTPlayer'
 
-const robotoFlex = Roboto_Flex({subsets: ['latin']})
+const robotoFlex = Roboto_Flex({ subsets: ['latin'] })
 export default function Home() {
   return (
     <RecoilRoot>
-      <main className={`${robotoFlex.className} flex min-h-screen flex-col items-center justify-between p-24`}>
+      <main
+        className={`${robotoFlex.className} flex min-h-screen flex-col items-center justify-between p-24`}
+      >
         <MainPage />
         <YTPlayer />
+        <Footer />
       </main>
     </RecoilRoot>
   )
